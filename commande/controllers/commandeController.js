2d4f7dbab72702a64e687762006cd8dde3e38894
Prince Thierry LOUBAYI MYSSIE <caviaros123@gmail.com>
2024-11-21 03:41:45 -0800

    Update commandeController.js
    Remove populate menuid on getting orders route

--- commande/controllers/commandeController.js
+++ commande/controllers/commandeController.js
@@ -47,7 +47,7 @@ module.exports.getOrders = async (req, res) => {
     
     
     try {
-        const orders = await Order.find().populate('menuId');
+        const orders = await Order.find();
         res.status(200).json(orders);
     } catch (error) {
         res.status(500).json({ message: 'Error getting orders', error });
